513131fafb446c34f5fd5b6ab8a02dc8312637e1
mdumar99 <uma000r@gmail.com>
2022-10-29 22:49:48 +0800

    MovieGoerUse, MovieGoer, Cineplex
    Edits:
    
    MovieGoerUse.java
    |__Implemented Goer SignUp/Login
         |__Note that you have run the program
               finish for the Goer to be saved
               in the .ser file
         |__Now can implement add reviews
               and booking and booking history
    
    MovieGoer.java
    |__Made it serialisabe to save multiple user
          to the .ser file
    
    Cineplex.java
    |__Added array of movieGoer

--- src/MovieGoer.java
+++ src/MovieGoer.java
@@ -1,10 +1,27 @@
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class MovieGoer {
+public class MovieGoer implements Serializable{
+	private int goerID;
 	private String name;
 	private int mobileNumber;
 	private String email;
 	private ArrayList<Booking> pastBooking = new ArrayList<Booking>();
+
+	MovieGoer(int goerID, String name, int number, String email){
+		setGoerID(goerID);
+		setName(name);
+		setEmail(email);
+		setMobileNumber(number);
+	}
+
+	public int getGoerID(){
+		return goerID;
+	}
+
+	private void setGoerID(int goerID){
+		this.goerID = goerID;
+	}
 	
 	public String getName() {
 		return name;
@@ -15,23 +32,14 @@ public class MovieGoer {
 	public int getMobileNumber() {
 		return mobileNumber;
 	}
-	public boolean setMobileNumber(int mobileNumber) {
-		if(String.valueOf(mobileNumber).length() == 8 && //check if number has 8 digits and starts with 8/9
-			(String.valueOf(mobileNumber).substring(0,1) == String.valueOf(8) || String.valueOf(mobileNumber).substring(0,1) == String.valueOf(9))) {
-			this.mobileNumber = mobileNumber;
-			return true;
-		}
-		return false;
+	public void setMobileNumber(int mobileNumber) {
+		this.mobileNumber = mobileNumber;
 	}
 	public String getEmail() {
 		return email;
 	}
-	public boolean setEmail(String email) {
-		if(email.contains("@") && email.contains(".com")) {
-			this.email = email;
-			return true;
-		}
-		return false;
+	public void setEmail(String email) {
+		this.email = email;
 	}
 	public ArrayList<Booking> getpastBooking() {
 		return pastBooking;
--- src/MovieGoerUse.java
+++ src/MovieGoerUse.java
@@ -1,18 +1,90 @@
 import java.util.Scanner;
-import java.util.random.RandomGenerator.ArbitrarilyJumpableGenerator;
-import java.util.ArrayList;
+import java.util.Objects;
 
 public class MovieGoerUse {
 
+	public static int GoerID;
+
 	public static Cineplex MovieGoerChoice(Cineplex cineplex) {
-		
-		//MovieGoerLogin();
+		Scanner sc = new Scanner (System.in);
+		boolean loginSuccess = false;
+		System.out.println("Select 1 if you are a new User.");
+		System.out.println("Select 2 if you are a current User.");
+		do{
+			int userChoice = sc.nextInt();
+			if(userChoice==1){
+				cineplex = MovieGoerSignup(cineplex);
+				loginSuccess = true;
+			}else if(userChoice==2){
+				cineplex = MovieGoerLogin(cineplex);
+				loginSuccess = true;
+			}else{
+				System.out.println("Invalid Entry. Try Again!");
+				System.out.println("Select 1 if you are a new User.");
+				System.out.println("Select 2 if you are a current User.");
+			}
+		}while(loginSuccess == false);
 		cineplex = GoerCinema(cineplex);
 		return cineplex;
 	}
 
-	public void MovieGoerLogin() {
+	public static Cineplex MovieGoerSignup(Cineplex cineplex){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("---------Sign Up-------------");
+		System.out.println("Enter Your name: ");
+		String name = sc.nextLine();
+		System.out.println("Enter Your Phone number: ");
+		int hpNumber = enterNumber();
+		System.out.println("Enter Your Email Address: ");
+		String emailAddr = enterEmail();
+		if(!cineplex.getListofGoers().isEmpty()){
+			GoerID = cineplex.getListofGoers().size() + 1;
+		}else{
+			GoerID = 0;
+		}
+		System.out.println();
+		cineplex.addGoer(GoerID ,name, hpNumber, emailAddr);
+		System.out.println("Welcome, "+ name + "!");
+		return cineplex;
+	}
+
+
+	public static Cineplex MovieGoerLogin(Cineplex cineplex) {
+		if(cineplex.getListofGoers().isEmpty()){
+			System.out.println("No users registered");
+			cineplex = MovieGoerSignup(cineplex);
+			return cineplex;
+		}
+		boolean loginSuccess = false;
+		boolean goerExists = false;
+		System.out.println("---------Log in-------------");
+		System.out.println("Enter Your Email Address: ");
 		
+		do{
+			String email = enterEmail();
+			for(int i=0;i<cineplex.getListofGoers().size();i++){
+				if(Objects.equals(cineplex.getGoer(i).getEmail(), email)){
+					goerExists = true;
+					GoerID = i;
+				}
+			}
+			if(goerExists==true){
+				System.out.println("Enter your phone Number: ");
+				int number = enterNumber();
+				if(number==cineplex.getGoer(GoerID).getMobileNumber()){
+					loginSuccess = true;
+					System.out.println("Welcome, " + cineplex.getGoer(GoerID).getName());
+				}else{
+					System.out.println("Phone number does not correspond to your email. Try Again!");
+				}
+				
+			}else{
+				System.out.println("User does not exist. Try Again!");
+			}
+		}while(loginSuccess==false);
+		
+
+		return cineplex;
 	}
 
 	private static Cineplex GoerCinema(Cineplex cineplex){
@@ -45,63 +117,23 @@ public class MovieGoerUse {
 			choice = sc.nextInt();
 			switch(choice){
 				case 1:// Search/List Movies
-					for(int i=0; i<cinema.getListOfMovie().size();i++){
-						System.out.println(" " + (i+1)+ ". " + cinema.getMovie(i).getMovieTitle());
-					}
-					System.out.println();
+					printListofMovies(cinema);
 					break;
 				case 2://View Movie Details
 					System.out.println("Select to see the details: ");
-					//printListOfMovies();
-					for(int i=0; i<cinema.getListOfMovie().size();i++){
-						System.out.println(" " + (i+1)+ ". " + cinema.getMovie(i).getMovieTitle());
-					}
-					System.out.println();
+					printListofMovies(cinema);
 					int movieChoice = sc.nextInt();
-					System.out.println("*************************");
-					System.out.println(cinema.getMovie(movieChoice-1).getMovieTitle() + ": ");
-					System.out.println(cinema.getMovie(movieChoice-1).getDirector() + ": ");
-					System.out.println(cinema.getMovie(movieChoice-1).getSynopsis() + ": ");
-					if(cinema.getMovie(movieChoice-1).getBlockBuster() == true) {
-						System.out.println("BlockBuster Movie" + ": ");
-					}
-					if(cinema.getMovie(movieChoice-1).getSneakpreview() == true) {
-						System.out.println("BlockBuster Movie" + ": ");
-					}
+					if(movieChoice<=cinema.getListOfMovie().size()){
+						printDetailsofCinema(cinema, movieChoice);
+					}else{
+						System.out.println("Invalid Input!");
+					}	
 					break;
 				case 3://Check Seat Availabliltiy
 					break;
-				case 4://Book and purchase tickets   //still have to do booking for this case
-					Payment Price = new Payment();
-					double total = 0;
-					for(int i=0; i<cinema.getListOfMovie().size();i++){// search/list movies
-						System.out.println(" " + (i+1)+ ". " + cinema.getMovie(i).getMovieTitle());
-					}
-					int mc = sc.nextInt();
-					System.out.println();
-					Price.PriceList(); //listing out pricing list
-					MovieType MT = Price.chooseMovieType();//choose MovieType
-					System.out.println("Select Number of Tickets:");
-					int noTick = sc.nextInt();
-					Tickets[] Ticketarray = new Tickets[noTick];
-					for(int a = 0; a<noTick;a++) {
-						/*Select & Book Seats Code here*/
-						//======================
-						TicketType TT = Price.chooseTicketType();
-						Ticketarray[a] = new Tickets(MT, TT, cinema.getMovie(mc-1).getBlockBuster(), cinema.getMovie(mc-1).getSneakpreview());
-					}
-					System.out.println("Total Price = " + Price.totalPrice(Ticketarray, noTick));
-					double payment = 0;
-					while(!Price.checkPaid()) {
-						System.out.println("Payment Received: ");
-						payment = sc.nextDouble();
-						total += payment;
-						Price.checkPayment(total);
-					}
-					System.out.println("Change Given: " + Price.getchange());
+				case 4://Book and purchase tickets
 					break;
-				case 5://View Booking History 
-					
+				case 5://View Boooking History 
 					break;
 				case 6://List top 5
 					break;
@@ -116,10 +148,52 @@ public class MovieGoerUse {
 		return cinema;
 	}
 
-	/*public static Cinema printListOfMovies(){
-		Cinema cinema = new Cinema();
-		
-	}*/
+	public static int enterNumber(){
+		Scanner sc = new Scanner(System.in);
+		int number;
+		while(true){
+			number = sc.nextInt();
+			if(String.valueOf(number).length() == 8 && //check if number has 8 digits and starts with 8/9
+			(Objects.equals(String.valueOf(number).substring(0,1), String.valueOf(8)) || Objects.equals(String.valueOf(number).substring(0,1), String.valueOf(9)))) {
+				break;
+			}else{
+				System.out.println("Invalid input. Please enter a vaild number. Make sure number starts with 8 or 9 and has 8 digits."); 
+			}
+		}
+		return number;
+	}
+
+	public static String enterEmail(){
+		Scanner sc = new Scanner(System.in);
+		String email;
+		while(true){
+			email = sc.nextLine();
+			if(email.contains("@") && email.contains(".com")) {
+				break;
+			}else{
+				System.out.println("Invalid input. Please enter a vaild Email address. Make sure it contains @ and .com.");
+			}
+		}
+		return email;
+	}
+
+	private static Cinema printListofMovies(Cinema cinema){
+		for(int i=0; i<cinema.getListOfMovie().size();i++){
+			System.out.println(" " + (i+1)+ ". " + cinema.getMovie(i).getMovieTitle());
+		}
+		System.out.println();
+		return cinema;
+	}
+
+	private static Cinema printDetailsofCinema(Cinema cinema, int movieChoice){
+		System.out.println("*************************");
+		System.out.println(cinema.getMovie(movieChoice-1).getMovieTitle() + ": ");
+		System.out.println("  Director: " + cinema.getMovie(movieChoice-1).getDirector());
+		System.out.println("  Synopsis: " + cinema.getMovie(movieChoice-1).getSynopsis());
+		System.out.println("  BlockBuster: "+ cinema.getMovie(movieChoice-1).getBlockBuster());
+		System.out.println("*************************");
+		return cinema;
+	}
 	
 	public static void printUIChoice(){
 		System.out.println("==============================");
@@ -131,8 +205,6 @@ public class MovieGoerUse {
 		System.out.println("6. List top 5");
 		System.out.println("7. Exit");
 		System.out.println("==============================");
-		
-		
 	}
 	
 	
