a1f197aefaca42bf48b5244c2399108d42cc6b76
gwee jia xiang <104158999+gwjxng@users.noreply.github.com>
2022-11-12 12:46:16 +0800

    javadoc

--- Enum/MovieType.java
+++ Enum/MovieType.java
@@ -1,13 +1,32 @@
-
+/**
+ * Enumeration for the types of movie
+ *
+ */
 public enum MovieType {
+	/**
+	 * Regular & Digital Movies
+	 */
 	RD("Regular & Digital"),
-	TD("3D"),
-	EM("Empty"),;
-
+	/**
+	 * 3D Movies
+	 */
+	TD("3D"),;
+	
+	/**
+	 * A string to hold the type of Movie
+	 */
 	private String MovieType;
+	/**
+	 * Construct an Object based on the input
+	 * @param Movie abbreviated string for Movie Type
+	 */
 	MovieType(String Movie){
 		this.MovieType = Movie;
 	}
+	/**
+	 * Get Movie Type
+	 * @return the abbreviated string of the Movie Type
+	 */
 	public String getMovieType() {
 		return MovieType;
 	}
--- Enum/TicketType.java
+++ Enum/TicketType.java
@@ -1,19 +1,55 @@
-
+/**
+ * Enumeration for the types of Tickets
+ *
+ */
 public enum TicketType {
+	/**
+	 * Senior Citizens
+	 */
 	SC("Senior Citizens"),
+	/**
+	 * Students
+	 */
 	ST("Students"),
+	/**
+	 * Monday to Wednesday
+	 */
 	MW("Mon - Wed"),
+	/**
+	 *Thursday
+	 */
 	TH("Thu"),
+	/**
+	 * Friday Before 6pm
+	 */
 	FB("Friday Before 6"),
+	/**
+	 * Friday After 6pm
+	 */
 	FA("Friday After 6"),
+	/**
+	 * Saturday and Sunday
+	 */
 	SS("Sat & Sun"),
-	CL("Preferred Credit & Loyalty Card"),
-	EM("Empty");
+	/**
+	 * Credit and Loyalty Cards
+	 */
+	CL("Preferred Credit & Loyalty Card"),;
+	/**
+	 * A string to hold the type of ticket
+	 */
 	private String TicketType;
-	
+	/**
+	 * Construct an Object of TicketType based on input
+	 * @param Ticket Abbreviated string for Ticket Type 
+	 */
 	TicketType(String Ticket){
 		this.TicketType = Ticket;
 	}
+	/**
+	 * Get Ticket Type
+	 * @return abbreviated string for Ticket Type
+	 */
 	public String getTicketType() {
 		return TicketType;
 	}
