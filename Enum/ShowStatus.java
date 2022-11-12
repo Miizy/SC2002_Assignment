58ea0f8ae8f11f2ea806a9ca2f0e733efda336c2
Miizy <miizyqq@gmail.com>
2022-11-12 21:37:50 +0800

    Update ShowStatus.java

--- Enum/ShowStatus.java
+++ Enum/ShowStatus.java
@@ -1,7 +1,22 @@
+/**
+ * Enumeration for the status of the movie
+ */
 public enum ShowStatus {
+	/**
+	 * Movie is not available now and will be coming soon
+	 */
 	cs("Coming Soon"),
+	/**
+	 * A preview of the movie is available
+	 */
 	pr("Preview"),
+	/**
+	 * Movie is now showing and users can purchase tickets
+	 */
 	ns("Now Showing"),
+	/**
+	 * Movie air time has ended and will no longer be available
+	 */
 	na("End Of Showing");
 	
 	private String showStatus;
